95f292b61a7669dc741585cfff39fc8d9ee720ab
Immortalisanto <tur.anthony91@gmail.com>
2023-07-27 17:38:01 +0200

    controller download du CV OK

--- controllers/download.js
+++ controllers/download.js
@@ -1,3 +1,14 @@
 exports.getCv = async (req, res, next) => {
-    res.send('Hello download/getCv !');
+    const pathFile = './assets/CV.pdf';
+    const nameFile = 'CV_anthony_tur.pdf';
+
+    res.download(
+        pathFile,
+        nameFile,
+        (error = {
+            if(error) {
+                res.status(500).send('Erreur lors du téléchargement du CV.');
+            },
+        })
+    );
 };
